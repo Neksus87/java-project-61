e70302b4f34e65da0775eea01b877bfe9d2fe766
denis greb <neksus87@gmail.com>
2024-09-06 08:09:33 +0700

    add app first mind game project step 1

---
+++ app/build.gradle.kts
@@ -0,0 +1,21 @@
+import org.gradle.kotlin.dsl.dependencies as dependencies1
+
+plugins {
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies1 {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
