beacb2b64bd3cb102f757d8cda1712902de1b45a
denis greb <neksus87@gmail.com>
2024-09-11 09:02:41 +0700

    Project 1 step 4 Code quality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,27 +1,42 @@
-import org.gradle.kotlin.dsl.dependencies as dependencies1
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     application
-    id("java")
+    jacoco
+    id("checkstyle")
+    id("io.freefair.lombok") version "8.6"
+    id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-group = "hexlet.code"
+group = "io.hexlet"
+
 version = "1.0-SNAPSHOT"
 
-application { mainClass.set("hexlet.code.App") }
+application { mainClass.set("io.hexlet.Application") }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
-dependencies1 {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
-tasks.getByName("run", JavaExec::class) {
-    standardInput = System.`in`
+tasks.named("check") {
+    dependsOn("checkstyleMain")
 }
+
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 public class Cli {
     public static void greetUser() {
-	Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-	System.out.println("Welcome to the Brain Games!");
-	System.out.print("May I have your name? ");
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
 
-	String name = scanner.nextLine();
+        String name = scanner.nextLine();
 
-	System.out.println("Hello, " + name + "!");
+        System.out.println("Hello, " + name + "!");
     }
 }
